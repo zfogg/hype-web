2c985615fd0294bf959a5d6c6fff9db9ae62ac08
Zach Fogg <zach.fogg@gmail.com>
2014-03-12 00:27:02 -0400

    pushing to waj

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,37 @@
+var hmData = {};
 $(function(){
 
     // heatmap configuration
+    var heatmap$ = document.getElementById("heatmapArea");
+    var canvas$ = document.getElementById("canvas");
     var config = {
-        element: document.getElementById("heatmapArea"),
-        radius: 30,
-        opacity: 50
+        element: heatmap$,
+        radius: 280,
+        opacity: 80
     };
+
+    var W = canvas$.width,
+        H = canvas$.height;
+
 
     //creates and initializes the heatmap
     var heatmap = h337.create(config);
 
-    // let's get some data
-    var data = {
-        max: 20,
-        data: [
-            { x: 10, y: 20, count: 18 },
-            { x: 25, y: 25, count: 14 },
-            { x: 50, y: 30, count: 20 }
-        ]
-    };
-
-    heatmap.store.setDataSet(data);
+    var dataRef = new Firebase('https://hypeapp.firebaseio.com');
+    dataRef.on('value', function(snapshot) {
+        var val = snapshot.val();
+        console.log(val);
+        var hmData = {
+            max: 100,
+            data: [
+                { x: W*1.5,
+                y:   H*2.0,
+                count: val*0.5 }
+            ]
+        };
+        $("#hypeScore").text(val);
+        heatmap.store.setDataSet(hmData);
+    });
 });
+
+
