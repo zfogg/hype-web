4d22a343e3195baa3a782a9aca3578cf9d81b124
Zach Fogg <zach.fogg@gmail.com>
2014-03-12 01:49:42 -0400

    assd

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,12 @@
 var hmData = {};
 $(function(){
 
-var dataRef = new Firebase('https://hypeapp.firebaseio.com');
-dataRef.on('value', function(snapshot) {
-    console.log('Hype: ' + snapshot.val());
-});
+    var dataRef = new Firebase('https://hypeapp.firebaseio.com');
 
-//harlem shake
-(function(){function c(){var e=document.createElement("link");e.setAttribute("type","text/css");e.setAttribute("rel","stylesheet");e.setAttribute("href",f);e.setAttribute("class",l);document.body.appendChild(e)}function h(){var e=document.getElementsByClassName(l);for(var t=0;t<e.length;t++){document.body.removeChild(e[t])}}function p(){var e=document.createElement("div");e.setAttribute("class",a);document.body.appendChild(e);setTimeout(function(){document.body.removeChild(e)},100)}function d(e){return{height:e.offsetHeight,width:e.offsetWidth}}function v(i){var s=d(i);return s.height>e&&s.height<n&&s.width>t&&s.width<r}function m(e){var t=e;var n=0;while(!!t){n+=t.offsetTop;t=t.offsetParent}return n}function g(){var e=document.documentElement;if(!!window.innerWidth){return window.innerHeight}else if(e&&!isNaN(e.clientHeight)){return e.clientHeight}return 0}function y(){if(window.pageYOffset){return window.pageYOffset}return Math.max(document.documentElement.scrollTop,document.body.scrollTop)}function E(e){var t=m(e);return t>=w&&t<=b+w}function S(){var e=document.createElement("audio");e.setAttribute("class",l);e.src=i;e.loop=false;e.addEventListener("canplay",function(){setTimeout(function(){x(k)},500);setTimeout(function(){N();p();for(var e=0;e<O.length;e++){T(O[e])}},15500)},true);e.addEventListener("ended",function(){N();h()},true);e.innerHTML=" <p>If you are reading this, it is because your browser does not support the audio element. We recommend that you get a new browser.</p> <p>";document.body.appendChild(e);e.play()}function x(e){e.className+=" "+s+" "+o}function T(e){e.className+=" "+s+" "+u[Math.floor(Math.random()*u.length)]}function N(){var e=document.getElementsByClassName(s);var t=new RegExp("\\b"+s+"\\b");for(var n=0;n<e.length;){e[n].className=e[n].className.replace(t,"")}}var e=30;var t=30;var n=350;var r=350;var i="http://s3.amazonaws.com/moovweb-marketing/playground/harlem-shake.mp3";var s="mw-harlem_shake_me";var o="im_first";var u=["im_drunk","im_baked","im_trippin","im_blown"];var a="mw-strobe_light";var f="//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake-style.css";var l="mw_added_css";var b=g();var w=y();var C=document.getElementsByTagName("*");var k=null;for(var L=0;L<C.length;L++){var A=C[L];if(v(A)){if(E(A)){k=A;break}}}if(A===null){console.warn("Could not find a node of the right size. Please try a different page.");return}c();S();var O=[];for(var L=0;L<C.length;L++){var A=C[L];if(v(A)){O.push(A)}}})()
+    dataRef.on('value', function(snapshot) {
+        console.log('Hype: ' + snapshot.val());
+    });
 
-//harlem shake
 
     // heatmap configuration
     var heatmap$ = document.getElementById("heatmapArea");
@@ -22,17 +19,16 @@ dataRef.on('value', function(snapshot) {
     };
 
     var W = canvas$.width * Math.random() + radius,
-        H = canvas$.height * Math.random() + radius;
+    H = canvas$.height * Math.random() + radius;
 
     //creates and initializes the heatmap
     var heatmap = h337.create(config);
 
-    var dataRef = new Firebase('https://hypeapp.firebaseio.com');
     dataRef.on('value', function(snapshot) {
         var obj = snapshot.val();
 
         var data = [];
-        
+
         // http://stackoverflow.com/questions/684672/loop-through-javascript-object
         var i = 1;
         for (var key in obj) {
